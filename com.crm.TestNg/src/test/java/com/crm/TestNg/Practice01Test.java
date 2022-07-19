821d8add23b62617a6a20f02f3db12c6ac724cf4
ATHAUR REHAMAN <96580337+ATHAURREHAMAN@users.noreply.github.com>
2022-07-19 15:13:23 +0530

    commit 05
    to upadte

--- com.crm.TestNg/src/test/java/com/crm/TestNg/Practice01Test.java
+++ com.crm.TestNg/src/test/java/com/crm/TestNg/Practice01Test.java
@@ -40,6 +40,7 @@ public class Practice01Test {
 		System.out.println("close method=04");
 		System.out.println("CLOSE THE CLASS");
 		System.out.println("Class ENDED");
-		System.out.println("============);
+		System.out.println("============");
+		System.out.println("++++++++++++);
 	}
 }
